e64c5f44a4046895aa0b011165dc92e4ffa96e92
RasyaJusticio <rasyajusticio.personal@gmail.com>
2025-06-22 15:39:00 +0700

    chore(services): rename lib folder into services

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,5 +1,5 @@
 import '../css/app.css';
-import './lib/i18n';
+import './services/i18n';
 
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
